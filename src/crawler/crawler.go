d8b9b256a848d127b005ed22f78fa4cca0853b7a
Adam Fordyce <adam.fordyce@capgemini.com>
2024-08-14 21:09:29 +0100

    Added the crawler package

--- src/crawler/crawler.go
+++ src/crawler/crawler.go
@@ -1,5 +1,10 @@
 package crawler
 
+// crawler package implements the Crawl method which accepts a seed url
+// All text/html content from the seed is parsed and links are scraped from
+// the html body, the calling function receives a list of all the links
+// that were found on the page that was crawled and scraped.
+
 import (
 	"bytes"
 	"fmt"
@@ -11,26 +16,51 @@ import (
 	"golang.org/x/net/html"
 )
 
+// The Crawler struct contains the seed domain to start crawling and the
+// channels used for error and standard output reporting.
 type Crawler struct {
 	Domain *url.URL
 	Out    chan<- string
 	Err    chan<- error
+	Fetch  chan<- *http.Response
 }
 
-func NewCrawler(domain string, output chan<- string, errors chan<- error) *Crawler {
+// NewCrawler, returns a pointer to a crawler.Crawler object, it is initialised
+// with a seed domain, output channel and an error channel
+func NewCrawler(domain string, output chan<- string, errors chan<- error, fetch chan<- *http.Response) *Crawler {
+	// The seed domain is stored in a url.URL object, so it is parsed to begin
+	// with, if there is an error then the code panics and exits so that it
+	// does not proceed with a bad "seed".
 	url, err := url.Parse(domain)
 	if err != nil {
 		errors <- fmt.Errorf("Error parsing domain")
 		panic(err)
 	}
+
 	return &Crawler{
 		Domain: url,
 		Out:    output,
 		Err:    errors,
+		Fetch:  fetch,
 	}
 }
 
-func (c *Crawler) CleanURL(rawUrl string) (string, error) {
+// The private cleanUrl - method will take a rawUrl that has been scraped
+// from an HTML page from the anchor nodes href attribute and run the following
+// steps on it.
+//   - Strip any leading or trailing spaces, this can confuse the net/url Parser
+//   - Detect if the link is a fragment i.e. #something, if it is then return the
+//     seed domain.
+//   - Detect if the link supplied is a relative path, if it is then rebuild the
+//     url from the seed domain i.e. /home/blog -> https://domain.com/home/blog
+//   - Use the net/url url.Parse method to load the url into a url.URL object
+//   - Check and ensure the domain in the URL is the same as the one supplied in
+//     the seed.
+//   - Ensure the protocol scheme is set on the URL, if not then use "https"
+//
+// Once all the checks have been complete, the url is reconstructed to ensure
+// there are no trailing `/` and to add any query string back onto it.
+func (c *Crawler) cleanUrl(rawUrl string) (string, error) {
 	rawUrl = strings.TrimSpace(rawUrl)
 
 	// If a fragment then return the host for the supplied domain
@@ -43,20 +73,32 @@ func (c *Crawler) CleanURL(rawUrl string) (string, error) {
 		rawUrl = c.Domain.Scheme + "://" + c.Domain.Hostname() + rawUrl
 	}
 
+	if strings.Contains(rawUrl, "127.0.0.1") && !strings.HasPrefix(rawUrl, "http") {
+		rawUrl = "http://" + rawUrl
+	}
+
+	if !strings.HasPrefix(rawUrl, "http") {
+		rawUrl = "https://" + rawUrl
+	}
+
 	// Parse the URL
 	u, err := url.Parse(rawUrl)
 	if err != nil {
 		return "", fmt.Errorf("Error parsing URL: %v", err)
 	}
 
-	// Check the requests hostname is in the same domain as the seed
-	if u.Hostname() != c.Domain.Hostname() {
-		return "", fmt.Errorf("request is for a subdomain or different domain")
-	}
-
 	// Set the protocol scheme to https if its not set
 	if u.Scheme == "" {
-		u.Scheme = "https"
+		if u.Host == "127.0.0.1" {
+			u.Scheme = "http"
+		} else {
+			u.Scheme = "https"
+		}
+	}
+
+	// Check the requests hostname is in the same domain as the seed
+	if u.Hostname() != c.Domain.Hostname() {
+		return "", nil
 	}
 
 	query := ""
@@ -73,6 +115,9 @@ func (c *Crawler) CleanURL(rawUrl string) (string, error) {
 	return u.Scheme + "://" + u.Host + path + query, nil
 }
 
+// fitleredLinks takes a list of URLs that may contain duplicates or empty
+// values. The function should remove any empty strings and de-duplicate the
+// entries, returning a list of unique URLs to the calling function.
 func filteredLinks(links []string) []string {
 	uniqueLinks := []string{}
 	seen := make(map[string]bool)
@@ -90,44 +135,71 @@ func filteredLinks(links []string) []string {
 	return uniqueLinks
 }
 
-func (c *Crawler) Crawl(url string) ([]string, error) {
-
+// The ProcessResponse method accepts the response from an http.Get request
+// The body is extracted from the response and processed to
+// locate all of the links in the html body.
+// If the content type is not as expected or the body is not able to be read
+// Then an error is returned
+func (c *Crawler) ProcessResponse(resp *http.Response) ([]string, error) {
+	defer resp.Body.Close()
+	url := resp.Request.URL.String()
 	var found []string
 
-	// Fetch the URL
-	//fmt.Printf("Fetching %s\n", url)
-	c.Out <- "Fetching " + url
-	resp, err := http.Get(url)
-	if err != nil {
-		return found, fmt.Errorf("Error fetching URL: %v", err)
-	}
-	defer resp.Body.Close()
-
-	header := resp.Header.Get("Content-Type")
-	if !strings.Contains(header, "text/html") {
-		return found, fmt.Errorf("URL is not for text/html: %s(%s)", url, header)
+	contentType := resp.Header.Get("Content-Type")
+	if !strings.Contains(contentType, "text/html") && !strings.Contains(contentType, "text/plain") {
+		return found, fmt.Errorf("%d,%s,Invalid Content Type: %s", resp.StatusCode, url, contentType)
 	}
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return found, fmt.Errorf("Error reading response body: %v", err)
+		return found, fmt.Errorf("%d,Error reading response body: %v", resp.StatusCode, err)
 	}
 
-	// Mark the URL as seen (early)
+	// Parse through the body and return all the links that have been found
 	links, err := c.startFindLinks(body)
 	if err != nil {
-		return found, fmt.Errorf("Error finding links: %v", err)
+		return found, fmt.Errorf("%d,Error finding links: %v", resp.StatusCode, err)
 	}
 
-	c.Out <- fmt.Sprintf("%s", url)
+	// Send all the unique links found to the output
 	for _, link := range filteredLinks(links) {
-		foundUrl, _ := c.CleanURL(link)
+		foundUrl, _ := c.cleanUrl(link)
 		found = append(found, foundUrl)
-		c.Out <- fmt.Sprintf(" - %s", link)
+		c.Out <- fmt.Sprintf("%d,%s,%s", resp.StatusCode, url, link)
 	}
+
+	// Return the found URLs to enqueue for future processing
 	return found, nil
 }
 
-func (c *Crawler) FindLinks(links []string, n *html.Node) []string {
+// startFindLinks takes the html body inside a []byte slice and get an
+// html.Node using html.Parse
+// - recurse through all the elements in the html.Node
+// - for each link that is discovered, clean the URLs
+// - return a []string with all the URLs discovered
+func (c *Crawler) startFindLinks(body []byte) ([]string, error) {
+	var links []string
+	doc, err := html.Parse(bytes.NewReader(body))
+	if err != nil {
+		return []string{}, fmt.Errorf("Error parsing HTML: %v", err)
+	}
+	for _, a := range c.findLinks(nil, doc) {
+		url, err := c.cleanUrl(a)
+		if err != nil {
+			// TODO: Do not ignore failed URL cleaning
+			continue
+		}
+		links = append(links, url)
+	}
+	return links, nil
+}
+
+// findLinks extracts all the anchor elements in an html node, extracts the
+// href attribute and updates the slice passed in with the links on it, it
+// the html node is looped over and if there are more children in the node, it
+// recurses calling itself until all the nodes have been seen and had their
+// links extracted.
+// It returns a slice with all the links that were found in the seed html node
+func (c *Crawler) findLinks(links []string, n *html.Node) []string {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
 			if a.Key != "href" {
@@ -137,24 +209,7 @@ func (c *Crawler) FindLinks(links []string, n *html.Node) []string {
 		}
 	}
 	for child := n.FirstChild; child != nil; child = child.NextSibling {
-		links = c.FindLinks(links, child)
+		links = c.findLinks(links, child)
 	}
 	return links
-}
-
-func (c *Crawler) startFindLinks(body []byte) ([]string, error) {
-	var links []string
-	doc, err := html.Parse(bytes.NewReader(body))
-	if err != nil {
-		return []string{}, fmt.Errorf("Error parsing HTML: %v", err)
-	}
-	for _, a := range c.FindLinks(nil, doc) {
-		url, err := c.CleanURL(a)
-		if err != nil {
-			// TODO: Do not ignore failed URL cleaning
-			continue
-		}
-		links = append(links, url)
-	}
-	return links, nil
 }
---
+++ src/crawler/crawler_test.go
@@ -0,0 +1,164 @@
+package crawler
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+var seedDomain string = "https://example.com"
+
+// test function for the cleanUrl method in the crawler package
+// take a list of URLs and test to make sure the output matches what
+// is expected.
+func Test_cleanUrl(t *testing.T) {
+	// key is the URL to test
+	// value is the expected result
+	testCases := map[string]string{
+		"example.com:8080":       "https://example.com:8080",
+		"example.com":            "https://example.com",
+		" example.com":           "https://example.com",
+		"example.com/":           "https://example.com",
+		"subdomain.example.com/": "",
+		" https://example.com":   "https://example.com",
+		"#fragment":              "https://example.com",
+		"/relative/path":         "https://example.com/relative/path",
+		"/":                      "https://example.com",
+		"google.com":             "",
+		"https://google.com":     "",
+	}
+
+	url, err := url.Parse(seedDomain)
+	if err != nil {
+		t.Errorf("Failed to parse seedDomain: %s", seedDomain)
+	}
+	c := Crawler{
+		Domain: url,
+	}
+
+	for url, expected := range testCases {
+		// Ignore the errors when calling cleanUrl, just want to test the output.
+		cleaned, err := c.cleanUrl(url)
+
+		if err != nil {
+			t.Errorf("cleaned URL [%s] failed: %v", url, err)
+		}
+
+		// Fail the test if there is a mismatch between expected and cleaned
+		if cleaned != expected {
+			t.Errorf("cleaned URL [%s] does not match the expected [%s]", cleaned, expected)
+		}
+	}
+}
+
+func Test_filteredLinks(t *testing.T) {
+	rawList := []string{
+		"https://example.com",
+		"https://example.com",
+		"https://example.com",
+		"https://example.com/path",
+		"https://example.com/path",
+		"https://example.com/path",
+	}
+
+	filteredList := filteredLinks(rawList)
+	if len(filteredList) != 2 {
+		t.Error("The filteredLinks test should only return two unique URLs")
+	}
+
+}
+
+func Test_startFindLinks(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		html := `
+		<html>
+		<body>
+		<p><a href="/">Link1</a></p>
+		<p><a href="/home">Link2</a></p>
+		<p><a href="/about">Link3</a></p>
+		<p><a href="/blog">Link4</a></p>
+		<p><a href="/contact">Link5</a></p>
+		<p><a href="/privacy">Link6</a></p>
+		<p><a href="/cookies">Link7</a></p>
+		</body>
+		</html>`
+		fmt.Fprintf(w, html)
+	}))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Error("Failed to get html from httptest server")
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Failed to get body from html response: %v", err)
+	}
+
+	output := make(chan string)
+	errors := make(chan error)
+	fetch := make(chan *http.Response)
+	c := NewCrawler(ts.URL, output, errors, fetch)
+	links, err := c.startFindLinks(body)
+	if err != nil {
+		t.Errorf("Failed to get links from sample html: %v", err)
+	}
+
+	if len(links) != 7 {
+		t.Errorf("Failed to get all links in sample html from %s: %d", ts.URL, len(links))
+	}
+
+}
+
+func Test_ProcessResponse(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		html := `
+		<html>
+		<body>
+		<p><a href="/">Link1</a></p>
+		<p><a href="/home">Link2</a></p>
+		<p><a href="/about">Link3</a></p>
+		<p><a href="/blog">Link4</a></p>
+		<p><a href="/contact">Link5</a></p>
+		<p><a href="/privacy">Link6</a></p>
+		<p><a href="/cookies">Link7</a></p>
+		</body>
+		</html>`
+		fmt.Fprintf(w, html)
+	}))
+	defer ts.Close()
+
+	output := make(chan string)
+	errors := make(chan error)
+	fetch := make(chan *http.Response)
+
+	go func() {
+		for {
+			select {
+			case msg := <-output:
+				fmt.Printf("%s\n", msg)
+			case err := <-errors:
+				fmt.Printf("%v\n", err)
+			}
+		}
+	}()
+	c := NewCrawler(ts.URL, output, errors, fetch)
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Error("Failed to get html from httptest server")
+	}
+
+	links, err := c.ProcessResponse(res)
+	if err != nil {
+		t.Errorf("Failed to get links from sample html: %v", err)
+	}
+
+	if len(links) != 7 {
+		t.Errorf("Failed to get all links in sample html from %s: %d", ts.URL, len(links))
+	}
+
+}
